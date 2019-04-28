aefd3375e4c198b41b8701f7aa20c3dec08daa6a
ms2sato <ms2.sato@gmail.com>
2019-04-28 13:43:26 +0900

    fix status literal

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   # GET /books/1
   # GET /books/1.json
   def show
-    unless @book.status == 1 && @book.published_at < Time.now
+    unless @book.status == "published" && @book.published_at < Time.now
       redirect_to root_path, notice: "アクセスできません"
     end
   end
